78f28c8f960528bc62bc33980f35b5ac3d5cd316
Rich Manalang <rmanalang@atlassian.com>
2013-05-03 12:22:25 -0700

    added more comments to app.js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-// Without Express, you've got nothing.
+// app.js is the entry point for your add-on. This bootstraps
+// your add-on server.
+
+// [Express](http://expressjs.com/) is your friend. It's the underlying
+// web framework that `feebs` uses.
 var express = require('express');
 
-// Bring in feebs
+// You need to load `feebs` to use her godly powers
 var feebs = require('feebs');
 
+// Typical web stuff you'll need later
 var http = require('http');
 var path = require('path');
 
-// Where all your routes live
+// Your routes live here. This is the C part of MVC.
 var routes = require('./routes');
 
 // Bootstrap Express
@@ -22,17 +27,17 @@ var port = addon.config.port();
 // Declares the environment to use in `config.js`
 var devMode = app.get('env') == "development";
 
-// all environments
+// The following settings applies to all environments.
 app.set('port', port);
 
 // All your views live here
 app.set('views', __dirname + '/views');
 
-// Jade is Express' preferred templating language,
-// but feel free to use a different one
+// Jade is Express' preferred templating language, but feel free to use a different one.
+// Check out the [consolidate](https://npmjs.org/package/consolidate) package
 app.set('view engine', 'jade');
 
-// Declare any Express [middleware](http://expressjs.com/api.html#middleware) you'd like to use
+// Declare any Express [middleware](http://expressjs.com/api.html#middleware) you'd like to use here
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -40,7 +45,7 @@ app.use(express.methodOverride());
 app.use(express.cookieParser());
 app.use(express.cookieSession({
   key: 'session',
-  secret: addon.config.secret()
+  secret: addon.config.secret() // Add your super secret salt in `config.js`
 }));
 // You need to instantiate the `feebs` middleware in order to get it's goodness for free
 app.use(addon.middleware());
